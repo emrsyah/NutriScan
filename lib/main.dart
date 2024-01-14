16a3c46a7ea7bdcdcddf44df1262a58caf387d07
Emirsyah <muhammademir48@gmail.com>
2024-01-14 22:56:36 +0700

    feat: setup flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nutriscan/core/routes/app_router.dart';
+import 'package:nutriscan/firebase_options.dart';
 
-void main() {
-  runApp(
-    ProviderScope(child: MyApp())
-  );
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
